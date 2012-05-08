71f4851fcc5c9d485093f96e67b2b2fd3e339658
Yasuharu NAKANO <ynak@jggug.org>
2012-05-08 17:25:15 +0900

    defined default package at Config.groovy

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,5 +1,7 @@
 // configuration for plugin testing - will not be included in the plugin zip
 
+grails.project.groupId = "jp.co.ntts.grails.plugin.domainlocking"
+
 log4j = {
     // Example of changing the log pattern for the default console
     // appender:
