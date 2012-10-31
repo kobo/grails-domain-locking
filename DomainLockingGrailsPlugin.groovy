a1289d688e2978d3dfde54f561d10d6c95101795
Yasuharu NAKANO <ynak@jggug.org>
2012-10-31 18:10:36 +0900

    arranged DomainLockingGrailsPlugin
    - bumped version 0.2
    - deleted unused commented lines
    - added some meta data

--- DomainLockingGrailsPlugin.groovy
+++ DomainLockingGrailsPlugin.groovy
@@ -4,13 +4,10 @@ import jp.co.ntts.grails.plugin.domainlocking.OptimisticLockingUtil
 import jp.co.ntts.grails.plugin.domainlocking.PessimisticLockingUtil
 
 class DomainLockingGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
+
+    def version = "0.2"
+    def grailsVersion = "2.1 > *"
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp",
         "grails-app/domain/test/**",
@@ -18,34 +15,14 @@ class DomainLockingGrailsPlugin {
         "web-app/**",
         "scripts/**",
     ]
-
-    // TODO Fill in these fields
-    def title = "Grails Domain Locking Plugin" // Headline display name of the plugin
-    def author = "NTT Software"
-    def authorEmail = ""
-    def description = '''\
-Brief summary/description of the plugin.
-'''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/grails-domain-locking"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def title = "Grails Domain Locking Plugin"
+    def author = "GGAO"
+    def license = "APACHE"
+    def description = 'This plugin provides a easy way for optimistic/pessimistic locking.'
+    def organization = [name: "NTT Software Coporation", url: "http://www.nttsoft.com/"]
+    def developers = [
+        [name: "Yasuharu NAKANO", email: "nakano.yasuharu@po.ntts.co.jp"],
+    ]
 
     def doWithDynamicMethods = { applicationContext ->
         for (domainClass in application.domainClasses) {
