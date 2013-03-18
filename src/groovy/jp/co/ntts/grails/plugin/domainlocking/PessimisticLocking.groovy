5f8628f1cab79be29e344542c2e4f9f56f00d6de
Yasuharu NAKANO <ynak@jggug.org>
2013-03-19 01:22:45 +0900

    translated source comments

--- src/groovy/jp/co/ntts/grails/plugin/domainlocking/PessimisticLocking.groovy
+++ src/groovy/jp/co/ntts/grails/plugin/domainlocking/PessimisticLocking.groovy
@@ -8,15 +8,14 @@ class PessimisticLocking {
     static withPessimisticLock(Class lockingDomainClass, Long lockingDomainId, Closure mainClosure) {
         Util.shouldNotNull(lockingDomainClass: lockingDomainClass, lockingDomainId: lockingDomainId, mainClosure: mainClosure)
 
-        // 1次キャッシュとDBの状態に差分がある場合(他のセッションによる変更・削除後など)、
-        // OptimisticLockingFailureExceptionが発生してしまう。これを防止するために、
-        // 強制的にflush & clearする。
+        // when there is different between 1st level cache and 2nd level cache (e.g. after other session did update or delete),
+        // OptimisticLockingFailureException occurs. To prevent it, session should be flush and clear forcely.
         lockingDomainClass.withSession { session ->
             session.flush()
             session.clear()
         }
 
-        // 悲観的ロックを取得する。
+        // acquire lock
         def lockedDomain = lockingDomainClass.lock(lockingDomainId)
         if (lockedDomain == null) {
             log.debug "Target not found: domainClass=${lockingDomainClass.name}, id=${lockingDomainId}"
@@ -29,7 +28,7 @@ class PessimisticLocking {
         }
         log.debug "Acquired pessimistic lock: domainClass=${lockingDomainClass.name}, id=${lockingDomainId}, version=${lockedDomain.version}"
 
-        // クロージャを実行する。
+        // execute main closure
         def returnValue = mainClosure.call(lockedDomain)
 
         return [
