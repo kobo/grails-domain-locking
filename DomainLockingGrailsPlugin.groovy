c5d957ff4ff651ad36da0aa7e22cb49982a5dc8b
Yasuharu NAKANO <ynak@jggug.org>
2013-03-29 18:09:53 +0900

    version 0.4-SNAPSHOT

--- DomainLockingGrailsPlugin.groovy
+++ DomainLockingGrailsPlugin.groovy
@@ -17,7 +17,7 @@ import org.jggug.kobo.domainlocking.PessimisticLocking
 
 class DomainLockingGrailsPlugin {
 
-    def version = "0.3"
+    def version = "0.4-SNAPSHOT"
     def grailsVersion = "2.0 > *"
     def pluginExcludes = [
         "grails-app/domain/test/TestDomain.groovy",
